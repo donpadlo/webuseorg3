abaf72cc5d654aef523c8073c6a231f934d56006
zerogle <zerogle@gmail.com>
2015-11-20 17:30:32 +0300

    Добавил экспорт выбранных записей отчёта "Список имущества" в CSV

--- controller/client/js/report.js
+++ controller/client/js/report.js
@@ -1,70 +1,135 @@
-function ListEqByPlaces(oid,pid,plpid){  
-  if ($("#gr").prop("checked")==false){
-  jQuery("#list2").jqGrid({
-   	url:'controller/server/reports/report.php?curuserid='+plpid+'&curorgid='+oid+'&curplid='+pid+'&tpo='+$("#sel_rep :selected").val()+'&os='+$("#os").prop("checked")+'&mode='+$("#mode").prop("checked")+'&repair='+$("#repair").prop("checked"),
-	datatype: "json",
-   	colNames:['Id','Помещение','Наименование','Группа','Инвентарник','Серийник','Штрихкод','Списан','ОС','Бух.имя'],
-   	colModel:[
-   		{name:'id',index:'id', width:20,hidden:true},
-                {name:'plname',index:'plname', width:110},
-   		{name:'namenome',index:'namenome', width:140},
-   		{name:'grname',index:'grname', width:140},                
-   		{name:'invnum',index:'invnum', width:100},
-   		{name:'sernum',index:'sernum', width:100},
-		{name:'shtrihkod',index:'shtrihkod', width:100},
-                {name:'mode',index:'mode', width:55,formatter: 'checkbox',edittype: 'checkbox'},
-                {name:'os',index:'os', width:55,formatter: 'checkbox',edittype: 'checkbox'},
-		{name:'buhname',index:'buhname', width:155}
-   	],
-        rownumbers: true,
-	autowidth: true,		
-	height: "auto",
-        shrinkToFit: false, 
-   	pager: '#pager2',
-   	sortname: 'plname',
-    viewrecords: true,
-    rowNum:1000,
-    scroll:1,
-    sortorder: "asc",    
-    caption:"Список имущества"
-});} else {
-    jQuery("#list2").jqGrid({
-   	url:'controller/server/reports/report.php?curuserid='+plpid+'&curorgid='+oid+'&curplid='+pid+'&tpo='+$("#sel_rep :selected").val()+'&os='+$("#os").prop("checked")+'&mode='+$("#mode").prop("checked")+'&repair='+$("#repair").prop("checked"),
-	datatype: "json",
-   	colNames:['Id','Помещение','Наименование','Группа','Инвентарник','Серийник','Штрихкод','Списан','ОС','Бух.имя'],
-   	colModel:[
-   		{name:'id',index:'id', width:20,hidden:true},
-                {name:'plname',index:'plname', width:110},
-   		{name:'namenome',index:'namenome', width:140},
-   		{name:'grname',index:'grname', width:140},                
-   		{name:'invnum',index:'invnum', width:100},
-   		{name:'sernum',index:'sernum', width:100},
-		{name:'shtrihkod',index:'shtrihkod', width:100},
-                {name:'mode',index:'mode', width:55,formatter: 'checkbox',edittype: 'checkbox'},
-                {name:'os',index:'os', width:55,formatter: 'checkbox',edittype: 'checkbox'},
-		{name:'buhname',index:'buhname', width:155}
-   	],
-   	grouping:true,
-   	groupingView : {
-            groupText : ['<b>{0} - {1} Item(s)</b>'],	    
-            groupColumnShow : [false],
-            groupField : ['grname']	    
-   	},	
-        rownumbers: true,
-	autowidth: true,		
-	height: "auto",
-        shrinkToFit: false, 
-   	pager: '#pager2',
-   	sortname: 'plname',
-    viewrecords: true,
-    rowNum:1000,
-    scroll:1,
-    sortorder: "asc",    
-    caption:"Список имущества"
-});
+function ListEqByPlaces(oid,pid,plpid){
+	if ($("#gr").prop("checked") == false){
+		jQuery("#list2").jqGrid({
+			url: 'controller/server/reports/report.php?curuserid='+plpid+'&curorgid='+oid+'&curplid='+pid+'&tpo='+$("#sel_rep :selected").val()+'&os='+$("#os").prop("checked")+'&mode='+$("#mode").prop("checked")+'&repair='+$("#repair").prop("checked"),
+			datatype: "json",
+			colNames:['Id','Помещение','Наименование','Группа','Инвентарник','Серийник','Штрихкод','Списан','ОС','Бух.имя'],
+			colModel:[
+				{name:'id',index:'id', width:20,hidden:true},
+				{name:'plname',index:'plname', width:110},
+				{name:'namenome',index:'namenome', width:140},
+				{name:'grname',index:'grname', width:140},                
+				{name:'invnum',index:'invnum', width:100},
+				{name:'sernum',index:'sernum', width:100},
+				{name:'shtrihkod',index:'shtrihkod', width:100},
+				{name:'mode',index:'mode', width:55,formatter: 'checkbox',edittype: 'checkbox'},
+				{name:'os',index:'os', width:55,formatter: 'checkbox',edittype: 'checkbox'},
+				{name:'buhname',index:'buhname', width:155}
+			],
+			rownumbers: true,
+			autowidth: true,
+			height: "auto",
+			shrinkToFit: false, 
+			pager: '#pager2',
+			sortname: 'plname',
+			viewrecords: true,
+			rowNum:1000,
+			scroll:1,
+			sortorder: 'asc',
+			caption:'Список имущества',
+			multiselect: true
+		});
+	} else {
+		jQuery("#list2").jqGrid({
+			url:'controller/server/reports/report.php?curuserid='+plpid+'&curorgid='+oid+'&curplid='+pid+'&tpo='+$("#sel_rep :selected").val()+'&os='+$("#os").prop("checked")+'&mode='+$("#mode").prop("checked")+'&repair='+$("#repair").prop("checked"),
+			datatype: "json",
+			colNames:['Id','Помещение','Наименование','Группа','Инвентарник','Серийник','Штрихкод','Списан','ОС','Бух.имя'],
+			colModel:[
+				{name:'id',index:'id', width:20,hidden:true},
+				{name:'plname',index:'plname', width:110},
+				{name:'namenome',index:'namenome', width:140},
+				{name:'grname',index:'grname', width:140},                
+				{name:'invnum',index:'invnum', width:100},
+				{name:'sernum',index:'sernum', width:100},
+				{name:'shtrihkod',index:'shtrihkod', width:100},
+				{name:'mode',index:'mode', width:55,formatter: 'checkbox',edittype: 'checkbox'},
+				{name:'os',index:'os', width:55,formatter: 'checkbox',edittype: 'checkbox'},
+				{name:'buhname',index:'buhname', width:155}
+			],
+			grouping:true,
+			groupingView : {
+				groupText : ['<b>{0} - {1} Item(s)</b>'],
+				groupColumnShow : [false],
+				groupField : ['grname']
+			},
+			rownumbers: true,
+			autowidth: true,
+			height: "auto",
+			shrinkToFit: false, 
+			pager: '#pager2',
+			sortname: 'plname',
+			viewrecords: true,
+			rowNum:1000,
+			scroll:1,
+			sortorder: "asc",    
+			caption:"Список имущества"
+		});
+	};
+	jQuery("#list2").jqGrid('navGrid','#pager2',{edit:false,add:false,del:false,search:false});
+	jQuery('#list2').jqGrid('navButtonAdd', '#pager2', {
+		id: 'ExportToCSV',
+		caption: 'Экспорт в CSV',
+		title: 'Экспорт в CSV',
+		onClickButton: function(e){
+			exportData(e, '#list2');
+		},
+		buttonicon: 'ui-icon ui-icon-document',
+	});
 };
-jQuery("#list2").jqGrid('navGrid','#pager2',{edit:false,add:false,del:false,search:false});
-};
+
+function exportData(e, id){
+	var gridid = jQuery(id).getDataIDs();
+	//var label = jQuery(id).getRowData(gridid[0]);
+	var selRowIds = jQuery(id).jqGrid('getGridParam', 'selarrrow');
+	var obj = new Object();
+	obj.count = selRowIds.length;
+	if (obj.count){
+		obj.items = new Array();
+		for (elem in selRowIds){
+			obj.items.push(jQuery(id).getRowData(selRowIds[elem]));
+		}
+		var json = JSON.stringify(obj);
+		JSONToCSVConvertor(json, 'csv', 1);
+	}
+}
+
+function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel){
+	var arrData = (typeof JSONData != 'object') ? JSON.parse(JSONData) : JSONData;
+	var CSV = '';
+	if (ShowLabel){
+		var row = '';
+		for (var index in arrData.items[0]){
+			row += index + ';';
+		}
+		row = row.slice(0, -1);
+		CSV += row + '\r\n';
+	}
+	for (var i = 0; i < arrData.items.length; i++){
+		var row = '';
+		for (var index in arrData.items[i]){
+			row += '"' + arrData.items[i][index].replace(/(<([^>]+)>)/ig, '') + '";';
+		}
+		row.slice(0, row.length - 1);
+		CSV += row + '\r\n';
+	}
+	if (CSV == ''){
+		alert('Invalid data');
+		return;
+	}
+
+	var link = document.createElement('a');
+	link.id = 'lnkDwnldLnk';
+	document.body.appendChild(link);
+	var csv = CSV;
+	blob = new Blob([csv], {type: 'text/csv'});
+	var myURL = window.URL || window.webkitURL;
+	jQuery('#lnkDwnldLnk').attr({
+		'download': 'Export.csv',
+		'href': myURL.createObjectURL(blob)
+	}); 
+	jQuery('#lnkDwnldLnk')[0].click();
+	document.body.removeChild(link);
+}
 
 function GetListPlaces(orgid,placesid){
        url="controller/server/common/getlistplaces.php?orgid="+orgid+"&placesid="+placesid+"&addnone=true";
