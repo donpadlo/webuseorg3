d0105a3895ae5f232986ff839c0f444cba6299dd
donpadlo <donpadlo@gmail.com>
2015-12-03 14:16:34 +0300

    - добавил обратно меню для СМС, забыл перенести при переделке в новое меню

--- inc/menu/sms_center.php
+++ inc/menu/sms_center.php
@@ -9,7 +9,7 @@
 
 $md=new Tmod; // обьявляем переменную для работы с классом модуля
 $md->Register("smscenter", "СМС-Центр", "Грибов Павел"); 
-if ($md->IsActive("smscenter")==1) {
+if ($md->IsActive("smscenter")==1) { 
     $this->Add("main","<img src=controller/client/themes/$cfg->theme/ico/email.png> СМС-Центр","СМС-Центр",3,"smscenter","");
      $this->Add("smscenter","Шаблоны СМС для LanBilling",">Шаблоны СМС для LanBilling",3,"smscenter/smstemplates","lanbilling/smstemplates");
      $this->Add("smscenter","Отправка СМС группе абонетов LanBilling","Отправка СМС группе абонетов LanBilling",3,"smscenter/sendgroup","lanbilling/sms/sendgroup");
@@ -17,6 +17,6 @@ if ($md->IsActive("smscenter")==1) {
      $this->Add("smscenter","Статистика по СМС",">Статистика по СМС",3,"smscenter/smsstat","smscenter/smsstat");
      $this->Add("smscenter","Настройка агентов отправки СМС","Настройка агентов отправки СМС",3,"smscenter/smsconfig","smscenter/smsconfig");     
      $this->Add("smscenter","Отправка СМС группе","Отправка СМС группе",3,"smscenter/smsconfig","smscenter/sendgroupsms");     
-     $this->Add("smscenter","Управление группами","Управление группами",3,"smscenter/smsconfig","smscenter/sms");          
+     $this->Add("smscenter","Управление группами","Управление группами для отправки СМС",3,"smscenter/smsconfig","smscenter/sms");          
 }
 unset($md);
