7b775e25822499063a57264bbacdd534f959a458
solodyagin <solodyagin@gmail.com>
2015-12-21 10:08:25 +0300

    В таблице "Группа номенклатуры" увеличил значение rowNum, т.к. при "rowNum: 10" не появляется вертикальный скролл и опция "scroll: 1" не работает

--- controller/client/js/libre_group.js
+++ controller/client/js/libre_group.js
@@ -1,63 +1,62 @@
-var addOptions={
-    top: 0, left: 0, width: 500
+var addOptions = {
+	top: 0, left: 0, width: 500
 };
-jQuery("#list2").jqGrid({
-   	url:'controller/server/tmc/libre_group.php',
-	datatype: "json",
-   	colNames:[' ','Id','Имя','Комментарий','Действия'],
-   	colModel:[
-   		{name:'active',index:'active', width:20},
-   		{name:'id',index:'id', width:55,hidden:true},
-   		{name:'name',index:'name', width:200,editable:true},
-   		{name:'comment',index:'comment', width:200,editable:true},
-		{name: 'myac', width:80, fixed:true, sortable:false, resize:false, formatter:'actions',formatoptions:{keys:true}}
-   	],
-	autowidth: true,			
-   	rowNum:10,	   	
-   	pager: '#pager2',
-   	sortname: 'id',
-	scroll:1,
+jQuery('#list2').jqGrid({
+	url: 'controller/server/tmc/libre_group.php',
+	datatype: 'json',
+	colNames: [' ', 'Id', 'Имя', 'Комментарий', 'Действия'],
+	colModel: [
+		{name: 'active', index: 'active', width: 20},
+		{name: 'id', index: 'id', width: 55, hidden: true},
+		{name: 'name', index: 'name', width: 200, editable: true},
+		{name: 'comment', index: 'comment', width: 200, editable: true},
+		{name: 'myac', width: 80, fixed: true, sortable: false, resize: false, formatter: 'actions', formatoptions: {keys: true}}
+	],
+	autowidth: true,
+	rowNum: 50,
+	pager: '#pager2',
+	sortname: 'id',
+	scroll: 1,
 	height: 140,
-    viewrecords: true,
-    sortorder: "asc",
-    editurl:"controller/server/tmc/libre_group.php",
-    caption:"Группы номенклатуры",
-	onSelectRow: function(ids) {
-		if(ids == null) {
-			ids=0;
-			if(jQuery("#list10_d").jqGrid('getGridParam','records') >0 )
-			{
-				jQuery("#list10_d").jqGrid('setGridParam',{url:"controller/server/tmc/libre_group_sub.php?q=1&groupid="+ids,page:1});
-				jQuery("#list10_d").jqGrid('setGridParam',{editurl:"controller/server/tmc/libre_group_sub.php?q=1&groupid="+ids,page:1})
-				.trigger('reloadGrid');				
-			}
-		} else {			
-			jQuery("#list10_d").jqGrid('setGridParam',{url:"controller/server/tmc/libre_group_sub.php?q=1&groupid="+ids,page:1});
-			jQuery("#list10_d").jqGrid('setGridParam',{editurl:"controller/server/tmc/libre_group_sub.php?q=1&groupid="+ids,page:1})
-			.trigger('reloadGrid');			
-		}
-	}    
-});
-jQuery("#list2").jqGrid('setGridHeight',$(window).innerHeight()/2);
-jQuery("#list2").jqGrid('navGrid','#pager2',{edit:false,add:true,del:false,search:false},{},addOptions,{},{multipleSearch:false},{closeOnEscape:true});
-
-jQuery("#list10_d").jqGrid({
-	height: 100,
-	autowidth: true,		
-   	url:'controller/server/tmc/libre_group_sub.php',
-	datatype: "json",
-   	colNames:[' ','Id', 'Параметр', 'Действия'],
-   	colModel:[
-   		{name:'active',index:'active', width:20},
-   		{name:'id',index:'id', width:55,hidden:true},
-   		{name:'name',index:'name', width:200,editable:true},
-		{name: 'myac', width:80, fixed:true, sortable:false, resize:false, formatter:'actions',formatoptions:{keys:true}}   		
-   	],
-   	rowNum:5,   	
-   	pager: '#pager10_d',
-   	sortname: 'id',
-	scroll:1,
 	viewrecords: true,
-	sortorder: "asc",
-	caption:"Параметры группы номенклатуры"
-}).navGrid('#pager10_d',{add:true,edit:false,del:false,search:false},{},addOptions,{},{multipleSearch:false},{closeOnEscape:true} );
+	sortorder: 'asc',
+	editurl: 'controller/server/tmc/libre_group.php',
+	caption: 'Группы номенклатуры',
+	onSelectRow: function(ids) {
+		if (ids == null) {
+			ids = 0;
+			if (jQuery('#list10_d').jqGrid('getGridParam', 'records') > 0) {
+				jQuery('#list10_d').jqGrid('setGridParam', {url: 'controller/server/tmc/libre_group_sub.php?q=1&groupid=' + ids, page: 1});
+				jQuery('#list10_d').jqGrid('setGridParam', {editurl: 'controller/server/tmc/libre_group_sub.php?q=1&groupid=' + ids, page: 1})
+						.trigger('reloadGrid');
+			}
+		} else {
+			jQuery('#list10_d').jqGrid('setGridParam', {url: 'controller/server/tmc/libre_group_sub.php?q=1&groupid=' + ids, page: 1});
+			jQuery('#list10_d').jqGrid('setGridParam', {editurl: 'controller/server/tmc/libre_group_sub.php?q=1&groupid=' + ids, page: 1})
+					.trigger('reloadGrid');
+		}
+	}
+});
+jQuery('#list2').jqGrid('setGridHeight', $(window).innerHeight() / 2);
+jQuery('#list2').jqGrid('navGrid', '#pager2', {edit: false, add: true, del: false, search: false}, {}, addOptions, {}, {multipleSearch: false}, {closeOnEscape: true});
+
+jQuery('#list10_d').jqGrid({
+	height: 100,
+	autowidth: true,
+	url: 'controller/server/tmc/libre_group_sub.php',
+	datatype: 'json',
+	colNames: [' ', 'Id', 'Параметр', 'Действия'],
+	colModel: [
+		{name: 'active', index: 'active', width: 20},
+		{name: 'id', index: 'id', width: 55, hidden: true},
+		{name: 'name', index: 'name', width: 200, editable: true},
+		{name: 'myac', width: 80, fixed: true, sortable: false, resize: false, formatter: 'actions', formatoptions: {keys: true}}
+	],
+	rowNum: 5,
+	pager: '#pager10_d',
+	sortname: 'id',
+	scroll: 1,
+	viewrecords: true,
+	sortorder: 'asc',
+	caption: 'Параметры группы номенклатуры'
+}).navGrid('#pager10_d', {add: true, edit: false, del: false, search: false}, {}, addOptions, {}, {multipleSearch: false}, {closeOnEscape: true});
--- controller/client/themes/bootstrap/tmc_group.php
+++ controller/client/themes/bootstrap/tmc_group.php
@@ -4,28 +4,25 @@
 // Изначальный автор данного кода - Грибов Павел
 // http://грибовы.рф
 
-if ($user->TestRoles("1,4,5,6")==true){
+if ($user->TestRoles('1,4,5,6')){
 ?>
 <div class="container-fluid">
-<div class="row">            
-<div class="col-xs-12 col-md-12 col-sm-12">
-<table id="list2"></table>
-<div id="pager2"></div>
-<table id="list10_d"></table>
-<div id="pager10_d"></div>
- </div>
- </div>
- </div>    
- <script type="text/javascript" src="controller/client/js/libre_group.js"></script>
+	<div class="row">            
+		<div class="col-xs-12 col-md-12 col-sm-12">
+			<table id="list2"></table>
+			<div id="pager2"></div>
+			<table id="list10_d"></table>
+			<div id="pager10_d"></div>
+		</div>
+	</div>
+</div>    
+<script src="controller/client/js/libre_group.js"></script>
 <?php
-}
- else {
+} else {
 ?>
 <div class="alert alert-error">
-  У вас нет доступа в данный раздел!
+	У вас нет доступа в данный раздел!
 </div>
 <?php
-    
 }
-
 ?>
