b670e04902755bd48a1fbd869826762786a1130e
Павел <@DESKTOP-TQHLOH4.lan>
2016-01-23 20:11:01 +0300

    - начато создание модуля "Чат поддержки"

--- controller/client/js/chat_users_config.js
+++ controller/client/js/chat_users_config.js
@@ -2,16 +2,15 @@
 // Разработчки: Грибов Павел, (добавляйте себя если что-то делали))
 // http://грибовы.рф
 
-alert(route);
 jQuery("#list_chat_users").jqGrid({    
    	url: route + 'controller/server/chat/get_sites_users.php?',
 	datatype: "json",
    	colNames:['Id','Имя','Логин','Привязка к сайтам','Действия'],
    	colModel:[
    		{name:'id',index:'id', width:55,hidden:true},
-   		{name:'name',index:'name', width:100,editable:false},
-   		{name:'login',index:'login', width:100,editable:false},
-                {name:'params',index:'params', width:200,editable:true},
+   		{name:'name',index:'name', width:100,editable:false,search: false},
+   		{name:'login',index:'login', width:100,editable:false,search: true},
+                {name:'params',index:'params', width:200,editable:true,search: false},
 		{name: 'myac', width:80, fixed:true, sortable:false, resize:false, formatter:'actions',formatoptions:{keys:true}}
    	],
 	autowidth: true,		
@@ -27,4 +26,5 @@ var addOptions={
     top: 0, left: 0, width: 500
 };
 jQuery("#list_chat_users").jqGrid('setGridHeight',$(window).innerHeight()/2);
-jQuery("#list_chat_users").jqGrid('navGrid','#pager_chat_users',{edit:true,add:true,del:false,search:false},{},addOptions,{},{multipleSearch:false},{closeOnEscape:true});
+jQuery("#list_chat_users").jqGrid('navGrid','#pager_chat_users',{edit:true,add:false,del:false,search:false},{},addOptions,{},{multipleSearch:false},{closeOnEscape:true});
+jQuery('#list_chat_users').jqGrid('filterToolbar', {stringResult: true, searchOnEnter: false});
--- controller/server/chat/get_sites_users.php
+++ controller/server/chat/get_sites_users.php
@@ -5,6 +5,8 @@
 //   (добавляйте себя если что-то делали)
 // http://грибовы.рф
 
+include_once(WUO_ROOT.'/class/cconfig.php'); // Класс настроек
+
 $oper= PostDef('oper');
 $name= PostDef('name');
 $login= PostDef('login');
@@ -16,10 +18,26 @@ $page = GetDef('page');
 $limit = GetDef('rows');
 $sidx = GetDef('sidx'); 
 $sord = GetDef('sord'); 
+$filters = GetDef('filters');
+
+	$flt = json_decode($filters, true);
+	$cnt = count($flt['rules']);
+	$where = '';
+	for ($i = 0; $i < $cnt; $i++) {
+		$field = $flt['rules'][$i]['field'];
+		$data = $flt['rules'][$i]['data'];
+		$where = $where."($field LIKE '%$data%')";
+		if ($i < ($cnt - 1)) {
+			$where = $where.' AND ';
+		}
+	}
+	if ($where != '') {
+		$where = 'WHERE '.$where;
+	}
 
 if ($oper==''){
 	if(!$sidx) $sidx =1;
-        $sql="SELECT COUNT(*) AS count FROM users where active=0";
+        $sql="SELECT COUNT(*) AS count FROM users $where";
         //echo "!$sql!";
 	$result = $sqlcn->ExecuteSQL($sql)  or die("Не могу выбрать количество записей!".mysqli_error($sqlcn->idsqlconnection));
 	$row = mysqli_fetch_array($result);
@@ -30,21 +48,29 @@ if ($oper==''){
             $total_pages = ceil($count/$limit);
             if ($page > $total_pages) $page=$total_pages;
             $start = $limit*$page - $limit;
-            $SQL = "SELECT * FROM users where active=0 ORDER BY $sidx $sord LIMIT $start , $limit";
-            $result = $sqlcn->ExecuteSQL( $SQL ) or die("Не могу выбрать список кабелей!".mysqli_error($sqlcn->idsqlconnection));            
+            $SQL = "SELECT * FROM users $where ORDER BY $sidx $sord LIMIT $start , $limit";
+//            echo "$SQL";
+            $result = $sqlcn->ExecuteSQL( $SQL ) or die("Не могу выбрать список пользователей!".mysqli_error($sqlcn->idsqlconnection));            
             $responce->page = $page;
             $responce->total = $total_pages;
             $responce->records = $count;
             $i=0;
+            $zu=new Tusers();
+            $cc=new Tcconfig();
             while($row = mysqli_fetch_array($result)) {
                     $responce->rows[$i]['id']=$row['id'];
-                    $responce->rows[$i]['cell']=array($row['id'],$row['name'],$row['login']);		
+                    $zu->GetById($row['id']);
+                    $params=$cc->GetByParam("user-chat-sites-".$row['id']);
+                    $responce->rows[$i]['cell']=array($row['id'],$zu->fio,$row['login'],$params);		
                     $i++;
             };
+            unset($zu);
+            unset($cc);
         };
 	echo json_encode($responce);
 };
 if ($oper=="edit"){
-  $sql="update lib_cable_spliter set name='$name',exitcount='$exitcount' where id='$id'";  
-  $result = $sqlcn->ExecuteSQL( $sql ) or die("Не могу обновить сплитер!".mysqli_error($sqlcn->idsqlconnection));            
+    $cc=new Tcconfig();
+    $cc->SetByParam("user-chat-sites-".$id,$params);
+    unset($cc);    
 };
