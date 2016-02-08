c8e06e722f5a8db9183fc13480025e6c3d04a2be
donpadlo <donpadlo@gmail.com>
2016-02-08 11:32:58 +0300

    - добавлен новый модуль - отслеживание запуска скриптов на серверах.

--- controller/server/devicescontrol/scriptalert.php
+++ controller/server/devicescontrol/scriptalert.php
@@ -20,6 +20,20 @@ $comment=_POST('comment');
 $current_alert_count=_POST('current_alert_count');
 $lastupdatedt=_POST('lastupdatedt');
 if ($oper==''){
+        $sql="CREATE TABLE IF NOT EXISTS `script_run_monitoring` (
+  `id` int(11) NOT NULL,
+  `script_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `group_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `alert_max_count` int(11) NOT NULL,
+  `alert_max_time` int(11) NOT NULL,
+  `lastupdatedt` datetime NOT NULL,
+  `current_alert_count` int(11) NOT NULL,
+  `sms_txt` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `active` tinyint(4) NOT NULL,
+  `sms_group_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4;";    
+    $result = $sqlcn->ExecuteSQL($sql);    
 	    /////////////////////////////
 	    // вычисляем фильтр
 	    /////////////////////////////
