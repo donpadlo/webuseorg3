70f24a892152ea7b2bc0ecb2df23ee52f3659551
Sergey Solodyagin <solodyagin@gmail.com>
2016-07-07 21:15:05 +0300

    Добавлена сортировка таблицы "Модули системы"

--- controller/client/js/mdconfig.js
+++ controller/client/js/mdconfig.js
@@ -1,24 +1,24 @@
-jQuery("#list2").jqGrid({
-   	url:'controller/server/config/md.php',
-	datatype: "json",
-   	colNames:['Id','Имя','Комментарий','Автор','Включено','Действия'],
-   	colModel:[
-   		{name:'id',index:'id', width:10,editable:false,hidden:true},   		
-   		{name:'name',index:'name', width:80,editable:false},
-                {name:'comment',index:'comment', width:100,editable:false},
-                {name:'copy',index:'copy', width:120,editable:false},
-		{name:'active',index:'active', width:30,editable:true,formatter: 'checkbox',edittype: 'checkbox', editoptions: {value: '1:0'}},
-		{name:'myac', width:80, fixed:true, sortable:false, resize:false, formatter:'actions',formatoptions:{keys:true}}
-   	],
-	autowidth: true,		
-   	pager: '#pager2',
-   	sortname: 'id',	
+jQuery('#list2').jqGrid({
+	url: route + 'controller/server/config/md.php',
+	datatype: 'json',
+	colNames: ['Id', 'Имя', 'Комментарий', 'Автор', 'Включено', 'Действия'],
+	colModel: [
+		{name: 'id', index: 'id', width: 10, editable: false, hidden: true},
+		{name: 'name', index: 'name', width: 80, editable: false},
+		{name: 'comment', index: 'comment', width: 100, editable: false},
+		{name: 'copy', index: 'copy', width: 120, editable: false},
+		{name: 'active', index: 'active', width: 30, editable: true, formatter: 'checkbox', edittype: 'checkbox', editoptions: {value: '1:0'}},
+		{name: 'myac', width: 80, fixed: true, sortable: false, resize: false, formatter: 'actions', formatoptions: {keys: true}}
+	],
+	autowidth: true,
+	pager: '#pager2',
+	sortname: 'name',
 	rowNum: 30,
-    viewrecords: true,
-    sortorder: "asc",
-    editurl:"controller/server/config/md.php",
-    caption:"Модули системы"
+	viewrecords: true,
+	sortorder: 'asc',
+	editurl: route + 'controller/server/config/md.php',
+	caption: 'Модули системы'
 });
 // загружаем навигационную панель
-jQuery("#list2").jqGrid('navGrid','#pager2',{edit:false,add:false,del:false,search:false});
-jQuery("#list2").jqGrid('setGridHeight',$(window).innerHeight()/2);
+jQuery('#list2').jqGrid('navGrid', '#pager2', {edit: false, add: false, del: false, search: false});
+jQuery('#list2').jqGrid('setGridHeight', $(window).innerHeight() / 2);
--- controller/server/config/md.php
+++ controller/server/config/md.php
@@ -1,4 +1,5 @@
 <?php
+
 // Данный код создан и распространяется по лицензии GPL v3
 // Разработчики:
 //   Грибов Павел,
@@ -6,86 +7,110 @@
 //   (добавляйте себя если что-то делали)
 // http://грибовы.рф
 
-include_once ("../../../config.php");                    // загружаем первоначальные настройки
-
-// загружаем классы
-
-include_once("../../../class/sql.php");               // загружаем классы работы с БД
-include_once("../../../class/config.php");		// загружаем классы настроек
-include_once("../../../class/users.php");		// загружаем классы работы с пользователями
-include_once("../../../class/employees.php");		// загружаем классы работы с профилем пользователя
-
-
-// загружаем все что нужно для работы движка
-
-include_once("../../../inc/connect.php");		// соеденяемся с БД, получаем $mysql_base_id
-include_once("../../../inc/config.php");              // подгружаем настройки из БД, получаем заполненый класс $cfg
-include_once("../../../inc/functions.php");		// загружаем функции
-include_once("../../../inc/login.php");		// загружаем функции
+defined('WUO_ROOT') or die('Доступ запрещён'); // Запрещаем прямой вызов скрипта.
 
 $page = GetDef('page');
-if ($page==0){$page=1;};
+if (empty($page)) {
+	$page = 1;
+}
 $limit = GetDef('rows');
-$sidx = GetDef('sidx'); 
-$sord = GetDef('sord'); 
-$oper= PostDef('oper');
+$sidx = GetDef('sidx', '1');
+$sord = GetDef('sord');
+$oper = PostDef('oper');
 $id = PostDef('id');
-$name= PostDef('name');
-$active= PostDef('active');
+$active = PostDef('active');
 
-if ($oper=='del')
-{
-	$result = $sqlcn->ExecuteSQL("SELECT * FROM config_common WHERE id='$id'") or die("Не могу выбрать список модулей!".mysqli_error($sqlcn->idsqlconnection));
-	while($row = mysqli_fetch_array($result)) {
-            $modname=$row['nameparam'];
-            $str1 = explode("_", $modname);
-            $mc='modulecomment_'.$str1[1];
-            $result2 = $sqlcn->ExecuteSQL("DELETE FROM config_common WHERE nameparam='$mc'")  or die("Не могу выбрать список комментарие!".mysqli_error($sqlcn->idsqlconnection));
-            $mcopy='modulecopy_'.$str1[1];
-            $result2 = $sqlcn->ExecuteSQL("DELETE FROM config_common WHERE nameparam='$mcopy'")  or die("Не могу выбрать список авторов!".mysqli_error($sqlcn->idsqlconnection));
-	}
-	$result = $sqlcn->ExecuteSQL("DELETE FROM config_common WHERE id='$id'") or die("Не могу выбрать список модулей!".mysqli_error($sqlcn->idsqlconnection));
-};    
-
-if ($oper=='edit')
-{
-	$SQL = "UPDATE config_common SET valueparam='$active' WHERE id='$id'";
-	$result = $sqlcn->ExecuteSQL( $SQL ) or die("Не могу обновить данные по модулю!".mysqli_error($sqlcn->idsqlconnection));
-};
-
-if ($oper=='')
-{
-	if(!$sidx) $sidx =1;
+if ($oper == '') {
+	// Проверяем может ли пользователь просматривать?
+	$user->TestRoles('1,3,4,5,6') or die('Недостаточно прав');
+	
 	$result = $sqlcn->ExecuteSQL("SELECT COUNT(*) AS count FROM config_common WHERE nameparam LIKE 'modulename_%'");
 	$row = mysqli_fetch_array($result);
 	$count = $row['count'];
-
-	if( $count >0 ) {$total_pages = ceil($count/$limit);} else {$total_pages = 0;};
-	if ($page > $total_pages) $page=$total_pages;
-
-	$start = $limit*$page - $limit;
-	$SQL = "SELECT * FROM config_common WHERE nameparam LIKE 'modulename_%' ORDER BY $sidx $sord LIMIT $start , $limit";
-        //echo "!$SQL!";
-	$result = $sqlcn->ExecuteSQL( $SQL ) or die("Не могу выбрать список модулей!".mysqli_error($sqlcn->idsqlconnection));
-        $responce=new stdClass();
+	$total_pages = ($count > 0) ? ceil($count / $limit) : 0;
+	if ($page > $total_pages) {
+		$page = $total_pages;
+	}
+	$start = $limit * $page - $limit;
+	$responce = new stdClass();
+	if ($start < 0) {
+		$responce->page = 0;
+		$responce->total = 0;
+		$responce->records = 0;
+		jsonExit($responce);
+	}
+//	$sql = "SELECT * FROM config_common WHERE nameparam LIKE 'modulename_%' ORDER BY $sidx $sord LIMIT $start, $limit";
+	$sql = <<<TXT
+SELECT t1.id `id`, SUBSTR(t1.nameparam, 12) AS `name`, t2.valueparam `comment`, t3.valueparam `copy`, t1.valueparam `active`
+FROM config_common t1
+LEFT JOIN config_common t2 ON (
+	SUBSTR(t2.nameparam, 15) = SUBSTR(t1.nameparam, 12) AND
+	t2.nameparam LIKE "modulecomment_%"
+)
+LEFT JOIN config_common t3 ON (
+	SUBSTR(t3.nameparam, 12) = SUBSTR(t1.nameparam, 12) AND
+	t3.nameparam LIKE "modulecopy_%"
+)
+WHERE t1.nameparam LIKE "modulename_%"
+ORDER BY $sidx $sord
+LIMIT $start, $limit
+TXT;
+	$result = $sqlcn->ExecuteSQL($sql)
+			or die('Не могу выбрать список модулей!' . mysqli_error($sqlcn->idsqlconnection));
 	$responce->page = $page;
 	$responce->total = $total_pages;
 	$responce->records = $count;
-	$i=0;
-	while($row = mysqli_fetch_array($result)) {
-            $modname=$row['nameparam'];
-            $str1 = explode("_", $modname);
-            $mc='modulecomment_'.$str1[1];
-            $result2 = $sqlcn->ExecuteSQL("SELECT * FROM config_common WHERE nameparam='$mc'")  or die("Не могу выбрать список комментарие!".mysqli_error($sqlcn->idsqlconnection));
-            while($row2 = mysqli_fetch_array($result2)) {$mc=$row2['valueparam'];};
-            $mcopy='modulecopy_'.$str1[1];
-            $result2 = $sqlcn->ExecuteSQL("SELECT * FROM config_common WHERE nameparam='$mcopy'")  or die("Не могу выбрать список авторов!".mysqli_error($sqlcn->idsqlconnection));
-            while($row2 = mysqli_fetch_array($result2)) {$mcopy=$row2['valueparam'];};            
-	    $responce->rows[$i]['id']=$row['id'];	    
-		$responce->rows[$i]['cell']=array($row['id'],$str1[1],$mc,$mcopy,$row['valueparam']);
-	    $i++;
+	$i = 0;
+	while ($row = mysqli_fetch_array($result)) {
+		$responce->rows[$i]['id'] = $row['id'];		
+//		$modname = $row['nameparam'];
+//		$str1 = explode('_', $modname);
+//		$mc = 'modulecomment_' . $str1[1];
+//		$result2 = $sqlcn->ExecuteSQL("SELECT * FROM config_common WHERE nameparam='$mc'")
+//				or die('Не могу выбрать список комментариев!' . mysqli_error($sqlcn->idsqlconnection));
+//		while ($row2 = mysqli_fetch_array($result2)) {
+//			$mc = $row2['valueparam'];
+//		}
+//		$mcopy = 'modulecopy_' . $str1[1];
+//		$result2 = $sqlcn->ExecuteSQL("SELECT * FROM config_common WHERE nameparam='$mcopy'")
+//				or die('Не могу выбрать список авторов!' . mysqli_error($sqlcn->idsqlconnection));
+//		while ($row2 = mysqli_fetch_array($result2)) {
+//			$mcopy = $row2['valueparam'];
+//		}
+//		$responce->rows[$i]['cell'] = array($row['id'], $str1[1], $mc, $mcopy, $row['valueparam']);
+		$responce->rows[$i]['cell'] = array($row['id'], $row['name'], $row['comment'], $row['copy'], $row['active']);
+		$i++;
 	}
 	echo json_encode($responce);
-};
+	exit;
+}
 
-?>
+if ($oper == 'edit') {
+	// Проверяем может ли пользователь редактировать?
+	$user->TestRoles('1,5') or die('Недостаточно прав');
+
+	$SQL = "UPDATE config_common SET valueparam='$active' WHERE id='$id'";
+	$result = $sqlcn->ExecuteSQL($SQL)
+			or die('Не могу обновить данные по модулю!' . mysqli_error($sqlcn->idsqlconnection));
+	exit;
+}
+
+if ($oper == 'del') {
+	// Проверяем может ли пользователь удалять?
+	$user->TestRoles('1,6') or die('Недостаточно прав');
+
+	$result = $sqlcn->ExecuteSQL("SELECT * FROM config_common WHERE id='$id'")
+			or die('Не могу выбрать список модулей!' . mysqli_error($sqlcn->idsqlconnection));
+	while ($row = mysqli_fetch_array($result)) {
+		$modname = $row['nameparam'];
+		$str1 = explode("_", $modname);
+		$mc = 'modulecomment_' . $str1[1];
+		$result2 = $sqlcn->ExecuteSQL("DELETE FROM config_common WHERE nameparam='$mc'")
+				or die('Не могу выбрать список комментариев!' . mysqli_error($sqlcn->idsqlconnection));
+		$mcopy = 'modulecopy_' . $str1[1];
+		$result2 = $sqlcn->ExecuteSQL("DELETE FROM config_common WHERE nameparam='$mcopy'")
+				or die('Не могу выбрать список авторов!' . mysqli_error($sqlcn->idsqlconnection));
+	}
+	$result = $sqlcn->ExecuteSQL("DELETE FROM config_common WHERE id='$id'")
+			or die('Не могу выбрать список модулей!' . mysqli_error($sqlcn->idsqlconnection));
+}
