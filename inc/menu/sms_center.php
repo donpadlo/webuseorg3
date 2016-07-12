a8a2d3fbdc9bd7f121a615b35892056149e1b865
donpadlo <donpadlo@gmail.com>
2016-07-12 14:26:35 +0300

    - Избавляемся от зависимостей от иконок. Удаляем лишние файлы.

--- inc/menu/sms_center.php
+++ inc/menu/sms_center.php
@@ -10,17 +10,17 @@
 $md=new Tmod; // обьявляем переменную для работы с классом модуля
 $md->Register("smscenter", "СМС-Центр", "Грибов Павел"); 
 if ($md->IsActive("smscenter")==1) { 
-    $this->Add("main","<img src=controller/client/themes/$cfg->theme/ico/email.png> СМС-Центр","СМС-Центр",3,"smscenter","");
-     $this->Add("smscenter","Отправка СМС по списку","Отправка СМС по списку",3,"smscenter/sendbylist","smscenter/sendbylist");
-     $this->Add("smscenter","Статистика по СМС",">Статистика по СМС",3,"smscenter/smsstat","smscenter/smsstat");
-     $this->Add("smscenter","Настройка агентов отправки СМС","Настройка агентов отправки СМС",3,"smscenter/smsconfig","smscenter/smsconfig");     
-     $this->Add("smscenter","Отправка СМС группе","Отправка СМС группе",3,"smscenter/smsconfig","smscenter/sendgroupsms");     
-     $this->Add("smscenter","Управление группами","Управление группами для отправки СМС",3,"smscenter/smsconfig","smscenter/sms");          
+    $this->Add("main","<i class=\"fa fa-commenting-o\" aria-hidden=\"true\"></i> СМС-Центр","СМС-Центр",3,"smscenter","");
+     $this->Add("smscenter","<i class=\"fa fa-comments-o\" aria-hidden=\"true\"></i> Отправка СМС по списку","Отправка СМС по списку",3,"smscenter/sendbylist","smscenter/sendbylist");
+     $this->Add("smscenter","<i class=\"fa fa-bar-chart\" aria-hidden=\"true\"></i> Статистика по СМС",">Статистика по СМС",3,"smscenter/smsstat","smscenter/smsstat");
+     $this->Add("smscenter","<i class=\"fa fa-cogs\" aria-hidden=\"true\"></i> Настройка агентов отправки СМС","Настройка агентов отправки СМС",3,"smscenter/smsconfig","smscenter/smsconfig");     
+     $this->Add("smscenter","<i class=\"fa fa-comments-o\" aria-hidden=\"true\"></i> Отправка СМС группе","Отправка СМС группе",3,"smscenter/smsconfig","smscenter/sendgroupsms");     
+     $this->Add("smscenter","<i class=\"fa fa-comments-o\" aria-hidden=\"true\"></i> Управление группами","Управление группами для отправки СМС",3,"smscenter/smsconfig","smscenter/sms");          
     if ($md->IsActive("lanbilling")==1) {
       // Меню для СМС, то что касается LanBilling
-	 $this->Add("smscenter","Отправка СМС группе абонетов LanBilling","Отправка СМС группе абонетов LanBilling",3,"smscenter/sendgroup","lanbilling/sms/sendgroup");     
-	 $this->Add("smscenter","Шаблоны СМС для LanBilling",">Шаблоны СМС для LanBilling",3,"smscenter/smstemplates","lanbilling/smstemplates");
-	 $this->Add("smscenter","Статистика по СМС",">Статистика по СМС",3,"smscenter/smsstat","smscenter/smsstat");  
+	 $this->Add("smscenter","<i class=\"fa fa-comments-o\" aria-hidden=\"true\"></i> Отправка СМС группе абонетов LanBilling","Отправка СМС группе абонетов LanBilling",3,"smscenter/sendgroup","lanbilling/sms/sendgroup");     
+	 $this->Add("smscenter","<i class=\"fa fa-code\" aria-hidden=\"true\"></i> Шаблоны СМС для LanBilling",">Шаблоны СМС для LanBilling",3,"smscenter/smstemplates","lanbilling/smstemplates");
+	 $this->Add("smscenter","<i class=\"fa fa-bar-chart\" aria-hidden=\"true\"></i> Статистика по СМС",">Статистика по СМС",3,"smscenter/smsstat","smscenter/smsstat");  
       // end     
     };     
 };
