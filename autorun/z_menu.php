b1b99090364c6a814c4bdd655d05c92b6b5cce36
donpadlo <donpadlo@gmail.com>
2016-11-29 13:35:42 +0300

    - кнопка меню "Удалить помеченные"

---
+++ autorun/z_menu.php
@@ -0,0 +1,11 @@
+<?php
+
+// Данный код создан и распространяется по лицензии GPL v3
+// Разработчики:
+//   Грибов Павел,
+//   (добавляйте себя если что-то делали)
+// http://грибовы.рф
+
+defined('WUO_ROOT') or die('Доступ запрещён'); // Запрещаем прямой вызов скрипта.
+$cfg->quickmenu[] = '<a title="Удалить помеченные на удаление обьекты" href="index.php?content_page=delete"><button type=\'button\' class=\'btn btn-default navbar-btn \'><i class="fa fa-trash-o"></i></button></a>';
+
