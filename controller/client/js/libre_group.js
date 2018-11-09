b78a18bbfe667a5b926fc2067e289324bd7df5fe
donpadlo <donpadlo@gmail.com>
2018-11-09 09:53:23 +0300

    - исправлена ошибка просмотра параметров группы номенклатуры

--- controller/client/js/libre_group.js
+++ controller/client/js/libre_group.js
@@ -2,7 +2,7 @@ var addOptions = {
 	top: 0, left: 0, width: 500
 };
 jQuery('#list2').jqGrid({
-	url: 'controller/server/tmc/libre_group.php',
+	url: route+'controller/server/tmc/libre_group.php',
 	datatype: 'json',
 	colNames: [' ', 'Id', 'Имя', 'Комментарий', 'Действия'],
 	colModel: [
@@ -20,19 +20,19 @@ jQuery('#list2').jqGrid({
 	height: 140,
 	viewrecords: true,
 	sortorder: 'asc',
-	editurl: 'controller/server/tmc/libre_group.php',
+	editurl: route+'controller/server/tmc/libre_group.php',
 	caption: 'Группы номенклатуры',
 	onSelectRow: function(ids) {
 		if (ids == null) {
 			ids = 0;
 			if (jQuery('#list10_d').jqGrid('getGridParam', 'records') > 0) {
-				jQuery('#list10_d').jqGrid('setGridParam', {url: 'controller/server/tmc/libre_group_sub.php?q=1&groupid=' + ids, page: 1});
-				jQuery('#list10_d').jqGrid('setGridParam', {editurl: 'controller/server/tmc/libre_group_sub.php?q=1&groupid=' + ids, page: 1})
+				jQuery('#list10_d').jqGrid('setGridParam', {url: route+'controller/server/tmc/libre_group_sub.php&q=1&groupid=' + ids, page: 1});
+				jQuery('#list10_d').jqGrid('setGridParam', {editurl: route+'controller/server/tmc/libre_group_sub.php&q=1&groupid=' + ids, page: 1})
 						.trigger('reloadGrid');
 			}
 		} else {
-			jQuery('#list10_d').jqGrid('setGridParam', {url: 'controller/server/tmc/libre_group_sub.php?q=1&groupid=' + ids, page: 1});
-			jQuery('#list10_d').jqGrid('setGridParam', {editurl: 'controller/server/tmc/libre_group_sub.php?q=1&groupid=' + ids, page: 1})
+			jQuery('#list10_d').jqGrid('setGridParam', {url: route+'controller/server/tmc/libre_group_sub.php&q=1&groupid=' + ids, page: 1});
+			jQuery('#list10_d').jqGrid('setGridParam', {editurl: route+'controller/server/tmc/libre_group_sub.php&q=1&groupid=' + ids, page: 1})
 					.trigger('reloadGrid');
 		}
 	}
@@ -43,7 +43,7 @@ jQuery('#list2').jqGrid('navGrid', '#pager2', {edit: false, add: true, del: fals
 jQuery('#list10_d').jqGrid({
 	height: 100,
 	autowidth: true,
-	url: 'controller/server/tmc/libre_group_sub.php',
+	url: route+'controller/server/tmc/libre_group_sub.php',
 	datatype: 'json',
 	colNames: [' ', 'Id', 'Параметр', 'Действия'],
 	colModel: [
@@ -52,7 +52,7 @@ jQuery('#list10_d').jqGrid({
 		{name: 'name', index: 'name', width: 200, editable: true},
 		{name: 'myac', width: 80, fixed: true, sortable: false, resize: false, formatter: 'actions', formatoptions: {keys: true}}
 	],
-	rowNum: 5,
+	rowNum: 50,
 	pager: '#pager10_d',
 	sortname: 'id',
 	scroll: 1,
--- controller/server/tmc/libre_group.php
+++ controller/server/tmc/libre_group.php
@@ -5,21 +5,6 @@
 // Сергей Солодягин (solodyagin@gmail.com)
 // (добавляйте себя если что-то делали)
 // http://грибовы.рф
-include_once ("../../../config.php"); // загружаем первоначальные настройки
-                                      
-// загружаем классы
-
-include_once ("../../../class/sql.php"); // загружаем классы работы с БД
-include_once ("../../../class/config.php"); // загружаем классы настроек
-include_once ("../../../class/users.php"); // загружаем классы работы с пользователями
-include_once ("../../../class/employees.php"); // загружаем классы работы с профилем пользователя
-                                              
-// загружаем все что нужно для работы движка
-
-include_once ("../../../inc/connect.php"); // соеденяемся с БД, получаем $mysql_base_id
-include_once ("../../../inc/config.php"); // подгружаем настройки из БД, получаем заполненый класс $cfg
-include_once ("../../../inc/functions.php"); // загружаем функции
-include_once ("../../../inc/login.php"); // загружаем функции
 
 $page = GetDef('page');
 $limit = GetDef('rows');
