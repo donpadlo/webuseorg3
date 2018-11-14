f6362d1257bcd877a8a1ed305b5921708692336d
donpadlo <donpadlo@gmail.com>
2018-11-14 15:25:03 +0300

    - исправление ошибок проверки прав на редактирование параметров (не было проверки)

--- controller/server/equipment/paramlist.php
+++ controller/server/equipment/paramlist.php
@@ -18,77 +18,47 @@ include_once ("../../../inc/config.php"); // подгружаем настрой
 include_once ("../../../inc/functions.php"); // загружаем функции
 include_once ("../../../inc/login.php"); // соеденяемся с БД, получаем $mysql_base_id
 
-if (isset($_GET["eqid"])) {
-    $id = $_GET['eqid'];
-} else {
-    $id = "";
-}
-;
-if (isset($_POST["oper"])) {
-    $oper = $_POST['oper'];
-} else {
-    $oper = "";
-}
-;
-if (isset($_POST["param"])) {
-    $param = $_POST['param'];
-} else {
-    $param = "";
-}
-;
-if (isset($_POST["id"])) {
-    $paramidid = $_POST['id'];
-} else {
-    $paramidid = "";
-}
-;
-if ($id == "") {
-    $id = $_POST['eqid'];
-}
-;
-// echo "!$paramid!";
-$responce = new stdClass();
-// получаем группу номенклатуры
-$SQL = "SELECT equipment.id,nome.id as nomeid,nome.groupid AS groupid FROM equipment INNER JOIN nome ON nome.id=equipment.nomeid WHERE (equipment.id='$id') AND (nome.active=1)";
-// echo "!$SQL!";
-$result = $sqlcn->ExecuteSQL($SQL) or die("Не получилось найти группу!" . mysqli_error($sqlcn->idsqlconnection));
-while ($row = mysqli_fetch_array($result)) {
-    $groupid = $row["groupid"];
-}
-;
-if ($groupid == "") {
-    echo "Нет параметров у группы!";
-    die();
-}
-;
-// echo "!$groupid!";
-// получаем список параметров группы
-$SQL = "SELECT id,name FROM group_param WHERE (groupid='$groupid') AND (active=1)";
-// echo "!$SQL!";
-$result = $sqlcn->ExecuteSQL($SQL) or die("Не получилось найти параметры!" . mysqli_error($sqlcn->idsqlconnection));
-while ($row = mysqli_fetch_array($result)) {
-    $paramid = $row["id"];
-    $name = $row["name"];
-    // echo "!$paramid!$name!";
-    // проверяем, если какогото параметра нет, то добавляем его в основную таблице связанную с оргнехникой
-    $SQL = "SELECT id FROM eq_param WHERE (grpid='$groupid') AND (eqid='$id') AND (paramid='$paramid')";
-    $res2 = $sqlcn->ExecuteSQL($SQL) or die("Не получилось выбрать существующие параметры!" . mysqli_error($sqlcn->idsqlconnection));
-    $cnt = 0;
-    while ($row2 = mysqli_fetch_array($res2)) {
-        $cnt ++;
-    }
-    ;
-    // если параметра нет, то добавляем...
-    if ($cnt == 0) {
-        $SQL = "INSERT INTO eq_param (id,grpid,paramid,eqid) VALUES (NULL,'$groupid','$paramid','$id')";
-        $rs = $sqlcn->ExecuteSQL($SQL);
-        if ($rs == '') {
-            die('Не смог добавить параметр!: ' . mysqli_error($sqlcn->idsqlconnection));
+if (isset($_GET["eqid"])) {$id = $_GET['eqid'];} else {$id = "";};
+if (isset($_POST["oper"])) {$oper = $_POST['oper'];} else {$oper = "";};
+if (isset($_POST["param"])) {$param = $_POST['param'];} else {$param = "";};
+if (isset($_POST["id"])) {$paramidid = $_POST['id'];} else {$paramidid = "";};
+if ($id == "") {$id = $_POST['eqid'];};
+
+if ($oper == '') {
+        $user->TestRoles('1,3,4,5,6') or die('Недостаточно прав');
+        $responce = new stdClass();
+        // получаем группу номенклатуры
+        $SQL = "SELECT equipment.id,nome.id as nomeid,nome.groupid AS groupid FROM equipment INNER JOIN nome ON nome.id=equipment.nomeid WHERE (equipment.id='$id') AND (nome.active=1)";
+        // echo "!$SQL!";
+        $result = $sqlcn->ExecuteSQL($SQL) or die("Не получилось найти группу!" . mysqli_error($sqlcn->idsqlconnection));
+        while ($row = mysqli_fetch_array($result)) {$groupid = $row["groupid"];};
+        if ($groupid == "") {echo "Нет параметров у группы!";die();};
+        // echo "!$groupid!";
+        // получаем список параметров группы
+        $SQL = "SELECT id,name FROM group_param WHERE (groupid='$groupid') AND (active=1)";
+        // echo "!$SQL!";
+        $result = $sqlcn->ExecuteSQL($SQL) or die("Не получилось найти параметры!" . mysqli_error($sqlcn->idsqlconnection));
+        while ($row = mysqli_fetch_array($result)) {
+            $paramid = $row["id"];
+            $name = $row["name"];
+            // echo "!$paramid!$name!";
+            // проверяем, если какогото параметра нет, то добавляем его в основную таблице связанную с оргнехникой
+            $SQL = "SELECT id FROM eq_param WHERE (grpid='$groupid') AND (eqid='$id') AND (paramid='$paramid')";
+            $res2 = $sqlcn->ExecuteSQL($SQL) or die("Не получилось выбрать существующие параметры!" . mysqli_error($sqlcn->idsqlconnection));
+            $cnt = 0;
+            while ($row2 = mysqli_fetch_array($res2)) {
+                $cnt ++;
+            };
+            // если параметра нет, то добавляем...
+            if ($cnt == 0) {
+                $SQL = "INSERT INTO eq_param (id,grpid,paramid,eqid) VALUES (NULL,'$groupid','$paramid','$id')";
+                $rs = $sqlcn->ExecuteSQL($SQL);
+                if ($rs == '') {
+                    die('Не смог добавить параметр!: ' . mysqli_error($sqlcn->idsqlconnection));
+                }
+            }
         }
-    }
-    ;
-}
-;
+};
 
 // получаем список параметров конкретной позиции
 $SQL = "SELECT eq_param.id as pid,group_param.name as pname,eq_param.param as pparam FROM eq_param INNER JOIN group_param ON group_param.id=eq_param.paramid WHERE (eqid='$id')";
@@ -104,10 +74,12 @@ while ($row = mysqli_fetch_array($result)) {
     );
     $i ++;
 }
-;
+
 echo json_encode($responce);
+
 // если просто листаем, тогда
 if ($oper == 'edit') {
+    $user->TestRoles('1,5') or die('Недостаточно прав');
     $sql = "UPDATE eq_param SET eq_param.param='$param' WHERE id='$paramidid'";
     // echo "!$sql!";
     $result = $sqlcn->ExecuteSQL($sql);
@@ -115,16 +87,17 @@ if ($oper == 'edit') {
         die('Не смог изменить параметр!: ' . mysqli_error($sqlcn->idsqlconnection));
     }
 }
-;
+
 
 if ($oper == 'del') {
+    $user->TestRoles('1,6') or die('Недостаточно прав');
     $sql = "DELETE FROM eq_param WHERE id='$paramidid'";
-    echo "!$sql!";
+ //   echo "!$sql!";
     $result = $sqlcn->ExecuteSQL($sql, $cfg->base_id);
     if ($result == '') {
         die('Не смог удалить параметр!: ' . mysqli_error($sqlcn->idsqlconnection));
     }
 }
-;
+
 
 ?>
